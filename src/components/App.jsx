8e834209bf1e353d106cf1e0935ac301d21d2a46
cristinamacicasan <cmacicasan@gmail.com>
2024-04-04 09:51:05 +0300

    change litera mare

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,13 @@
 import react from 'react';
-import profile from './Profile/profile';
+import Profile from './Profile/Profile';
 import userData from './Profile/user.json';
-import Statistics from './statistics/statistics';
+import Statistics from './statistics/Statistics';
 import statsData from './statistics/data.json';
-import PropTypes from 'prop-types';
 
 export const App = () => {
   return (
-    <div>
-      <profile
+    <>
+      <Profile
         username={userData.username}
         tag={userData.tag}
         location={userData.location}
@@ -16,19 +15,8 @@ export const App = () => {
         stats={userData.stats}
       />
       <Statistics title="Upload stats" stats={statsData} />
-    </div>
+    </>
   );
 };
 
 
-App.propTypes = {
-  username: PropTypes.string.isRequired,
-  tag: PropTypes.string.isRequired,
-  location: PropTypes.string.isRequired,
-  avatar: PropTypes.string.isRequired,
-  stats: PropTypes.shape({
-    followers: PropTypes.number.isRequired,
-    views: PropTypes.number.isRequired,
-    likes: PropTypes.number.isRequired,
-  }).isRequired,
-};
