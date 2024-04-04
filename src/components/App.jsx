de1fdf7676bf3e21848c0405c4d1488094c50e92
cristinamacicasan <cmacicasan@gmail.com>
2024-04-04 08:47:11 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@ import profile from './Profile/profile';
 import userData from './Profile/user.json';
 import Statistics from './statistics/statistics';
 import statsData from './statistics/data.json';
+import PropTypes from 'prop-types';
 
 export const App = () => {
   return (
@@ -17,4 +18,17 @@ export const App = () => {
       <Statistics title="Upload stats" stats={statsData} />
     </div>
   );
+};
+
+
+App.propTypes = {
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  stats: PropTypes.shape({
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
+  }).isRequired,
 };
